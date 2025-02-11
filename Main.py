e742e00eb215047a0806b2b6c8ef211b7ae94c4a
Owen Cooper <odc2106@columbia.edu>
2025-02-11 06:00:40 -0500

    Setting up the environment with openant for ant+  connection, serial for usb stick, and matplot for plotting the data

---
+++ Main.py
@@ -0,0 +1,8 @@
+
+import time
+import serial
+import matplotlib.pyplot as plt
+from openant.easy.node import Node
+from openant.easy.channel import Channel
+
+USB_PORT = "" #when i get the ANT+ dongle it will go here
