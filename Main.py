dd6cf11d0f510814ae40a82eb7aebcf1b55856e6
Owen Cooper <odc2106@columbia.edu>
2025-02-11 09:20:08 -0500

    Setting up the connection for ant+  connection using mostly standard numbers

--- Main.py
+++ Main.py
@@ -6,3 +6,17 @@ from openant.easy.node import Node
 from openant.easy.channel import Channel
 
 USB_PORT = "" #when i get the ANT+ dongle it will go here
+
+HRM_DEVICE_TYPE = 120  #120 is the hr sensor
+HRM_CHANNEL_PERIOD = 8070  # standard ANT+ heart rate period
+NETWORK_KEY = [0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45]  # public ANT+ network key
+
+node = Node(serial_device=USB_PORT)
+channel = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
+
+channel.set_period(HRM_CHANNEL_PERIOD)
+channel.set_search_timeout(255)
+channel.set_rf_freq(57) #standard frequency
+channel.set_network_key(0, NETWORK_KEY)
+channel.set_id(0, HRM_DEVICE_TYPE, 0)
+
