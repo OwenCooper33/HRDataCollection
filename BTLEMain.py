fdfe981220e3536a143b7d31bfff7940546c5766
Owen Cooper <odc2106@columbia.edu>
2025-02-14 16:49:46 -0500

    working on btle connection for the hr data

--- BTLEMain.py
+++ BTLEMain.py
@@ -10,25 +10,15 @@ HRM_SERVICE_UUID = '0000180d-0000-1000-8000-00805f9b34fb'
 HRM_CHAR_UUID = '00002a37-0000-1000-8000-00805f9b34fb'
 
 time_stamps = []
-heart_rate_data = []
 rr_intervals = []
 async def hr_data_handler(sender, data):
 # gets the hr from the hr sensro incoming data
 # checks the "flag" byte to see if the hr is stored in one or two
 # bytes and reads in the appropriate byte
     flag = data[0]
-    index = 1
-    if flag & 0x01:
-        hr_value = int.from_bytes(data[index:index+2], byteorder='little')
-        index += 2
-    else:
-        hr_value = data[index]
-        index += 1
-    heart_rate_data.append(hr_value)
-    time_stamps.append(time.time())
-
-    if flag & 0x10:  # RR-Interval present
-        while index < len(data):
-            rr = int.from_bytes(data[index:index+2], byteorder='little') / 1024.0  # Convert to seconds
-            rr_intervals.append(rr)
-            index += 2
+    if flag & 0x10:
+        rr_bytes = data[2:]
+        for i in range(0, len(rr_bytes), 2):
+            rr_interval = int.from_bytes(rr_bytes[i:i+2], byteorder='little') / 1000.0
+            rr_intervals.append(rr_interval)
+            time_stamps.append(time.time())
